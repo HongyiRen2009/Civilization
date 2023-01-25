9ca95b2811f9ab1e6ac87b5e8e462c23b6a8ade7
TNTiger19 <95938466+TNTiger17@users.noreply.github.com>
2023-01-24 20:22:39 -0800

    Update tech.js

--- js/tech.js
+++ js/tech.js
@@ -1,233 +1,236 @@
 //tech is a 2d array. The index of the array determines its level on the tree. To add something to requires, get the level of the object, then the index of it in the array. Category determines the column it'll
 //be put on
 const tech = [
-    [
-        {
-            name:"Housing optimization",
-			image:"<img src = 'images/techBlueprint.png' width='50' height='50'></img>",
-            description:"All housing options permanently become 20% cheaper, not including canopies.</br>TIER I",
+    [	//ROW 0
+		{
+            name:"Medium farm",
+            description:"Unlock a larger, more efficient medium farm",
             requires:[],
-            category:"I",
-            acquired: false,
             cost:0,
-            effect(){
-				resources+=900
-			}
+            category:"Farming",
+            acquired: false,
+            effect(){}
         },
 		{
-            name:"Discipline",
-            description:"During revolts, you now have the option to discipline the revolters,</br> losing 5% food production for 5 turns.</br>TIER II",
-            requires:[[0,0]],
-            category:"II",
-            acquired: false,
+            name:"Efficient Infrastructure",
+            description:"All housing options are now 30% cheaper",
+            requires:[],
             cost:0,
-            effect(){
-				resources+=900
-			}
+            category:"Housing",
+            acquired: false,
+            effect(){}
+        },
+		{
+            name:"Planned mines",
+            description:"All mines produce 50% resources",
+            requires:[],
+            cost:0,
+            category:"Mining",
+            acquired: false,
+            effect(){}
+        }
+    ],	
+	[	//ROW 1
+		{
+            name:"Discipline",
+            description:"During revolts, you are now able to punish revolters, losing 5% food production for 5 turns",
+            requires:[[0,0]],
+            cost:0,
+            category:"Virtue",
+            acquired: false,
+            effect(){}
+        },
+		{
+            name:"Workshop",
+            description:"Unlock a building that steadily increases your intelligence while draining resources.",
+            requires:[[0,2]],
+            cost:0,
+            category:"Intelligence",
+            acquired: false,
+            effect(){}
+        },
+		{
+            name:"Efficient space usage",
+            description:"All houses have 50% increased capacity",
+            requires:[[0,1],[0,0], [0,2]],
+            cost:0,
+            category:"Housing",
+            acquired: false,
+            effect(){}
+        }
+	],
+	[	//ROW 2
+		{
+            name:"Medium mine",
+            description:"Unlock a larger, more efficient medium mine",
+            requires:[[0,2],[1,2]],
+            cost:0,
+            category:"Mining",
+            acquired: false,
+            effect(){}
+        },
+		{
+            name:"Social care",
+            description:"-50% chance of rebellions and +30% total intelligence production",
+            requires:[[1,1]],
+            cost:0,
+            category:"Intelligence",
+            acquired: false,
+            effect(){}
+        },
+		{
+            name:"Military base",
+            description:"Unlock a larger structure that gives increased military power",
+            requires:[[1,1]],
+            cost:0,
+            category:"Military",
+            acquired: false,
+            effect(){}
+        }
+	],
+	[	//ROW 3
+		{
+            name:"Simple farms",
+            description:"Farms are 30% cheaper",
+            requires:[[0,0], [1,0], [1,2]],
+            cost:0,
+            category:"Farming",
+            acquired: false,
+            effect(){}
         },
 		{
             name:"Insulae",
-            description:"Unlock a huge housing unit.</br>TIER III",
-            requires:[[0,1]],
-            category:"III",
-            acquired: false,
-            cost:0,
-            effect(){
-				resources+=900
-			}
-        },
-		{
-            name:"Don't ask",
-            description:"Gain population 30% faster.</br>TIER VI",
-            requires:[[0,1]],
-            category:"VI",
-            acquired: false,
-            cost:0,
-            effect(){
-				resources+=900
-			}
-        }
-      
-    ],
-    [
-		{
-            name:"Virtuous",
-            description:"+50% reputation from market purchases.</br>TIER II",
-            requires:[[0,0]],
-            category:"II",
-            acquired: false,
-            cost:0,
-            effect(){
-				resources+=900
-			}
-        },
-		{
-            name:"Books!",
-            description:"All research sources are 40% more efficient.</br>TIER III",
-            requires:[[1,0]],
-            category:"III",
-            acquired: false,
-            cost:0,
-            effect(){
-				resources+=900
-			}
-        },
-		{
-            name:"Advanced housing",
-            description:"All housing options have 50% increased capacity, not including canopies.</br>TIER VI",
-            requires:[[1,1],[0,2]],
-            category:"VI",
-            acquired: false,
-            cost:0,
-            effect(){
-				resources+=900
-			}
-        },
-		{
-            name:"Exploration",
-            description:"Unlock the ability to reveal much more terrain using WASD.</br>TIER IV",
+            description:"Unlock a massive housing unit",
             requires:[[1,2]],
-            category:"IV",
+            cost:0,
+            category:"Housing",
             acquired: false,
+            effect(){}
+        },
+		{
+            name:"Scouting",
+            description:"Unlock a new outpost building that allows you to gain information about attackers",
+            requires:[[2,2]],
             cost:0,
-            effect(){
-				resources+=900
-			}
+            category:"Military",
+            acquired: false,
+            effect(){}
+        },
+		{
+            name:"Advanced tactics",
+            description:"New military bases give +40% military",
+            requires:[[2,2]],
+            cost:0,
+            category:"Military II",
+            acquired: false,
+            effect(){}
         }
-    ],
-    [
-        {
-            name:"Medium farms",
-            description:"Unlock a new larger type of farm that produces more food.</br>TIER I",
-            requires:[],
+	],	
+	[	//ROW 4
+		{
+            name:"Mining tacics",
+            description:"All mines are now 30% cheaper",
+            requires:[[0,2],[2,1]],
             cost:0,
-            category:"I",
+            category:"Mining",
+            acquired: false,
+            effect(){}
+        },
+		{
+            name:"Temple",
+            description:"Unlock a massive building to pray to RNGesus. Increases your luck",
+            requires:[[3,0],[1,0]],
+            cost:0,
+            category:"Virtue",
+            acquired: false,
+            effect(){}
+        },
+		{
+            name:"Large Farm",
+            description:"Unlock a substantially larger farm that requires irrigation",
+            requires:[[3,0]],
+            cost:0,
+            category:"Farming",
+            acquired: false,
+            effect(){}
+        },
+		{
+            name:"Fortress",
+            description:"Unlock a massive structure that offers substantial military power",
+            requires:[[3,3]],
+            cost:0,
+            category:"Military II",
+            acquired: false,
+            effect(){}
+        }
+	],
+	[	//ROW 5
+		{
+            name:"Large Mine",
+            description:"Unlock a massive mine that produces considerably more resources",
+            requires:[[4,0]],
+            cost:0,
+            category:"Mining",
+            acquired: false,
+            effect(){}
+        },
+		{
+            name:"Cement",
+            description:"All roads and bridges now only cost 0.25 resources",
+            requires:[[2,1]],
+            cost:0,
+            category:"Intelligence",
             acquired: false,
             effect(){}
         },
 		{
             name:"Efficient farms",
-            description:"All new farms produce 20% more resources.</br>TIER II",
-            requires:[[2,0]],
+            description:"All farms now produce 50% more resources",
+            requires:[[3,1],[4,2]],
             cost:0,
-            category:"II",
+            category:"Farming",
             acquired: false,
             effect(){}
         },
 		{
-            name:"Compact farms",
-            description:"All mines now require 20% less resources.</br>TIER III",
-            requires:[[2,1]],
+            name:"Archery",
+            description:"Unlock archer towers, a cheap building that gives military power, and all fortresses give +30% military power",
+            requires:[[3,2]],
             cost:0,
-            category:"III",
+            category:"Military",
             acquired: false,
             effect(){}
-        }
-    ],
-	[
+        },
 		{
             name:"Diplomacy",
-            description:"When negotiating with an attacker, gain a 60% chance to create a peace treaty-</br> reduces chance of attacks by 40% for the next 5 years.</br>TIER II",
-            requires:[[2,0]],
-            cost:0,
-            category:"II",
-            acquired: false,
-            effect(){}
-        },
-		{
-            name:"Large farms",
-            description:"Unlock a massive farm the requires irrigation.</br>TIER III",
-            requires:[[3,0]],
-            cost:0,
-            category:"III",
-            acquired: false,
-            effect(){}
-        },
-		{
-            name:"Negotiation",
-            description:"When in the market, you now have the option to randomize a trade-</br>20% chance to spend the cost to randomize.</br>TIER VI",
-            requires:[[3,1]],
-            cost:0,
-            category:"VI",
-            acquired: false,
-            effect(){}
-        }
-	],
-	[
-		{
-            name:"Planned mines",
-            description:"All mines are now 20% cheaper.</br>TIER I",
-            requires:[],
-            cost:0,
-            category:"I",
-            acquired: false,
-            effect(){}
-        },
-		{
-            name:"Material advantage",
-            description:"For every 10 resources you have, gain 5 military,</br>TIER II",
-            requires:[[4,0]],
-            cost:0,
-            category:"II",
-            acquired: false,
-            effect(){}
-        },
-		{
-            name:"Temples",
-            description:"Unlock a temple to increase your luck.</br>TIER VI",
-            requires:[[3,1]],
-            cost:0,
-            category:"VI",
-            acquired: false,
-            effect(){}
-        },
-		{
-            name:"Pickpockets",
-            description:"When buying from the black market, gain 20 resources.</br>TIER III",
+            description:"WHen negotiating with an enemy, gain a 60% chance that the enemy will create a peace treaty with you, decreasing chance of attacks by 40% for the next 5 years",
             requires:[[4,1]],
             cost:0,
-            category:"III",
-            acquired: false,
-            effect(){}
-        },
-		{
-            name:"Scientific breakthrough",
-            description:"Gain 1 blueprint scrap for the mega temple.</br>TIER IV",
-            requires:[[4,3]],
-            cost:0,
-            category:"IV",
+            category:"Virtue",
             acquired: false,
             effect(){}
         }
 	],
-	[
+	[ // ROW 7
 		{
-            name:"Efficient mines",
-            description:"All new mines now produce 20% more resources.</br>TIER II",
-            requires:[[4,0]],
-            cost:0,
-            category:"II",
-            acquired: false,
-            effect(){}
-        },
-		{
-            name:"Medium mines",
-            description:"Unlock a larger, more effective mine.</br>TIER III",
-            requires:[[5,0]],
-            cost:0,
-            category:"III",
-            acquired: false,
-            effect(){}
-        },
-		{
-            name:"Large mines",
-            description:"Unlock an extremely efficient large mine.</br>TIER VI",
+            name:"Rain wanted a special name for this i forgor",
+            description:"Unlock a blueprint scrap for the Mega Temple",
             requires:[[5,1]],
             cost:0,
-            category:"VI",
+            category:"Intelligence",
+            acquired: false,
+            effect(){}
+        },
+		{
+            name:"Exploration",
+            description:"Unlock infinitely more terrain to explore using WASD",
+            requires:[[5,4],[5,2],[3,1],[5,0]],
+            cost:0,
+            category:"Housing",
             acquired: false,
             effect(){}
         }
 	],
+	
     
 ]
