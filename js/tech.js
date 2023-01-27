1b37f57158f2dc9881389ef6c31e94b7e567186e
TNTiger19 <95938466+TNTiger17@users.noreply.github.com>
2023-01-27 08:53:50 -0800

    Update tech.js

--- js/tech.js
+++ js/tech.js
@@ -6,7 +6,7 @@ const tech = [
             name:"Medium farm",
             description:"Unlock a larger, more efficient medium farm",
             requires:[],
-            cost:0,
+            cost:1,
             category:"Farming",
             unlocked: false,
             effect(){}
@@ -22,10 +22,19 @@ const tech = [
         },
 		{
             name:"Planned mines",
-            description:"All mines produce 50% resources",
+            description:"All mines produce +50% resources",
             requires:[],
             cost:0,
             category:"Mining",
+            unlocked: false,
+            effect(){}
+        },
+		{
+            name:"Military base",
+            description:"Unlock a larger structure that gives increased military power",
+            requires:[],
+            cost:0,
+            category:"Military II",
             unlocked: false,
             effect(){}
         }
@@ -51,7 +60,7 @@ const tech = [
         },
 		{
             name:"Efficient space usage",
-            description:"All houses have 40% increased capacity",
+            description:"All houses have 50% increased capacity",
             requires:[[0,1],[0,0], [0,2]],
             cost:0,
             category:"Housing",
@@ -71,7 +80,7 @@ const tech = [
         },
 		{
             name:"Social care",
-            description:"-40% chance of rebellions and +30% total intelligence production",
+            description:"-50% chance of rebellions and +30% total intelligence production",
             requires:[[1,1]],
             cost:0,
             category:"Intelligence",
@@ -79,9 +88,9 @@ const tech = [
             effect(){}
         },
 		{
-            name:"Military base",
-            description:"Unlock a larger structure that gives increased military power",
-            requires:[[1,1]],
+            name:"Advanced tactics",
+            description:"New military bases give +40% military",
+            requires:[[1,1],[0,3]],
             cost:0,
             category:"Military",
             unlocked: false,
@@ -114,12 +123,12 @@ const tech = [
             cost:0,
             category:"Military",
             unlocked: false,
-            effect(){}
+            effect(){} 
         },
 		{
-            name:"Advanced tactics",
-            description:"New military bases give +40% military",
-            requires:[[2,2]],
+            name:"Stronger swords",
+            description:"All new military buildings give +20% military",
+            requires:[[2,2],[0,3]],
             cost:0,
             category:"Military II",
             unlocked: false,
@@ -162,6 +171,15 @@ const tech = [
             category:"Military II",
             unlocked: false,
             effect(){}
+        },
+		{
+            name:"Cement",
+            description:"All roads and bridges now only cost 0.10 resources",
+            requires:[[2,1]],
+            cost:0,
+            category:"Intelligence",
+            unlocked: false,
+            effect(){}
         }
 	],
 	[	//ROW 5
@@ -175,17 +193,8 @@ const tech = [
             effect(){}
         },
 		{
-            name:"Cement",
-            description:"All roads and bridges now cost 75% less resources",
-            requires:[[2,1]],
-            cost:0,
-            category:"Intelligence",
-            unlocked: false,
-            effect(){}
-        },
-		{
             name:"Efficient farms",
-            description:"All farms now produce 40% more resources",
+            description:"All farms now produce 50% more resources",
             requires:[[3,1],[4,2]],
             cost:0,
             category:"Farming",
@@ -199,7 +208,8 @@ const tech = [
             cost:0,
             category:"Military",
             unlocked: false,
-            effect(){}
+            effect(){
+			}
         },
 		{
             name:"Diplomacy",
@@ -210,28 +220,44 @@ const tech = [
             unlocked: false,
             effect(){}
         },
-	{
+		{
             name:"Exploration",
             description:"Unlock infinitely more terrain to explore using WASD",
             requires:[[4,4]],
-            cost:0,
-            category:"Housing",
-            unlocked: false,
-            effect(){}
-        }
-	],
-	[ // ROW 7
-	{
-            name:"The Ultimate Megastructure",
-            description:"Unlock a blueprint scrap for the Mega Temple",
-            requires:[[5,1]],
             cost:0,
             category:"Intelligence",
             unlocked: false,
             effect(){}
         }
-
 	],
-	
+	[ // ROW 6
+		{
+            name:"Rain wanted a special name for this i forgor",
+            description:"Unlock a blueprint scrap for the Mega Temple",
+            requires:[[3,1]],
+            cost:0,
+            category:"Housing",
+            unlocked: false,
+            effect(){}
+        },
+		{
+            name:"Exploration II",
+            description:"The world border is invisible, revealing more terrain for scouting</br>roads now cost 0.05 resources instead",
+            requires:[[5,4],[3,2]],
+            cost:0,
+            category:"Intelligence",
+            unlocked: false,
+            effect(){}
+        },
+			{
+            name:"Hunting grounds",
+            description:"Unlock the ability to create hunting grounds that produce food",
+            requires:[[5,2],[3,3]],
+            cost:0,
+            category:"Military",
+            unlocked: false,
+            effect(){}
+        }
+	],
     
 ]
