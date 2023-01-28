b54bd8de724700e2a30f21ea4fa19c0307c05b20
TNTiger19 <95938466+TNTiger17@users.noreply.github.com>
2023-01-27 21:36:30 -0800

    hope i didin't break everything

--- js/tech.js
+++ js/tech.js
@@ -3,18 +3,83 @@
 const tech = [
     [	//ROW 0
 		{
-            name:"Medium farm",
-            description:"Unlock a larger, more efficient medium farm",
+            name:"Efficient farms",
+            description:"All new farms produce +30% resources",
             requires:[],
-            cost:1,
+            cost:0,
             category:"Farming",
             unlocked: false,
             effect(){}
         },
 		{
-            name:"Efficient Infrastructure",
-            description:"All housing options are now 30% cheaper",
+            name:"Efficient space usage",
+            description:"New houses have +30% capacity",
             requires:[],
+            cost:0,
+            category:"Housing",
+            unlocked: false,
+            effect(){}
+        },
+		{
+            name:"Mining tactics",
+            description:"New mines produce +30% resources",
+            requires:[],
+            cost:0,
+            category:"Mining",
+            unlocked: false,
+            effect(){}
+        },
+		{
+            name:"Swords",
+            description:"All new military buildings give +20% military",
+            requires:[],
+            cost:0,
+            category:"Military",
+            unlocked: false,
+            effect(){}
+        },
+		{
+            name:"Charisma",
+            description:"Reputation sources are all 20% more efficient",
+            requires:[],
+            cost:0,
+            category:"Virute",
+            unlocked: false,
+            effect(){}
+        }
+    ],	
+	[	//ROW 1
+		{
+            name:"Medium farms",
+            description:"Unlock the blueprints for a larger medium mine",
+            requires:[[0,0]],
+            cost:0,
+            category:"Farming",
+            unlocked: false,
+            effect(){}
+        },
+		{
+            name:"Simple farms",
+            description:"All farms are 30% cheaper",
+            requires:[[0,0]],
+            cost:0,
+            category:"Farming II",
+            unlocked: false,
+            effect(){}
+        },
+		{
+            name:"Townhouse",
+            description:"Unlock the blueprints for a larger townhouse",
+            requires:[[0,1]],
+            cost:0,
+            category:"Housing",
+            unlocked: false,
+            effect(){}
+        },
+		{
+            name:"Efficient infrastructure",
+            description:"All houses are 30% cheaper",
+            requires:[[0,1]],
             cost:0,
             category:"Housing",
             unlocked: false,
@@ -22,86 +87,111 @@ const tech = [
         },
 		{
             name:"Planned mines",
-            description:"All mines produce +30% resources",
-            requires:[],
+            description:"All mines are 30% cheaper",
+            requires:[[0,2]],
             cost:0,
             category:"Mining",
             unlocked: false,
             effect(){}
         },
 		{
-            name:"Military base",
-            description:"Unlock a larger structure that gives increased military power",
-            requires:[],
+            name:"Medium mines",
+            description:"Unlock the blueprints for a larger medium mine",
+            requires:[[0,2]],
             cost:0,
-            category:"Military II",
+            category:"Mining II",
             unlocked: false,
             effect(){}
-        }
-    ],	
-	[	//ROW 1
+        },
 		{
-            name:"Discipline",
-            description:"During revolts, you are now able to punish revolters, losing 5% food production for 5 turns",
-            requires:[[0,0]],
+            name:"Military base",
+            description:"Unlock the blueprints for a more effective military base",
+            requires:[[0,3]],
+            cost:0,
+            category:"Military",
+            unlocked: false,
+            effect(){}
+        },
+		{
+            name:"Diplomacy",
+            description:"When negotiating with an enemy-</br>gain a 60% chance for the enemy to create a peace treaty with you for 5 years</br>during the peace treaty, -40% chance of enemy attacks",
+            requires:[[0,4]],
             cost:0,
             category:"Virtue",
             unlocked: false,
             effect(){}
         },
 		{
-            name:"Workshop",
-            description:"Unlock a building that steadily increases your intelligence while draining resources.",
-            requires:[[0,2]],
+            name:"Science",
+            description:"All intelligence sources are 30% more effective",
+            requires:[],
+            cost:0,
+            category:"Intelligence",
+            unlocked: false,
+            effect(){}
+        },
+	],
+	[	//ROW 2
+		{
+            name:"Exploration",
+            description:"Unlock infinitely more terrain to explore using WASD",
+            requires:[[1,8]],
             cost:0,
             category:"Intelligence",
             unlocked: false,
             effect(){}
         },
 		{
-            name:"Efficient space usage",
-            description:"All houses have 30% increased capacity",
-            requires:[[0,1],[0,0], [0,2]],
+            name:"Workshop",
+            description:"Unlock the blueprints for a workshop that constantly gives you intelligence at the cost of resources",
+            requires:[[1,8]],
             cost:0,
-            category:"Housing",
-            unlocked: false,
-            effect(){}
-        }
-	],
-	[	//ROW 2
-		{
-            name:"Medium mine",
-            description:"Unlock a larger, more efficient medium mine",
-            requires:[[0,2],[1,2]],
-            cost:0,
-            category:"Mining",
+            category:"Intelligence II",
             unlocked: false,
             effect(){}
         },
 		{
             name:"Social care",
-            description:"-30% chance of rebellions and +30% total intelligence production",
-            requires:[[1,1]],
+            description:"-30% chance of rebellions and gain population 20% faster",
+            requires:[[1,7]],
             cost:0,
-            category:"Intelligence",
+            category:"Virtue II",
             unlocked: false,
             effect(){}
         },
 		{
-            name:"Advanced tactics",
-            description:"New military bases give +40% military",
-            requires:[[1,1],[0,3]],
+            name:"Scouting",
+            description:"Gain the ability to get an estimate of attacking enemies' powers</br>Unlock blueprints for a scouting tower</br>increases the accuracy of the enemy power estimations",
+            requires:[[1,6]],
             cost:0,
-            category:"Military",
+            category:"Military II",
+            unlocked: false,
+            effect(){}
+        },
+		{
+            name:"Fishing",
+            description:"Unlock the blueprints for a fishing hut</br>Must be on a river and produces varying amounts of food",
+            requires:[[1,1],[1,3]],
+            cost:0,
+            category:"Farming II",
             unlocked: false,
             effect(){}
         }
 	],
 	[	//ROW 3
 		{
-            name:"Simple farms",
-            description:"Farms are 30% cheaper",
-            requires:[[0,0], [1,0], [1,2]],
+            name:"Cement",
+            description:"Roads and bridges now only cost 0.10 resources",
+            requires:[[2,0]],
+            cost:0,
+            category:"Intelligence",
+            unlocked: false,
+            effect(){}
+        },
+		{
+            name:"Large farm",
+            description:"Unlock the blueprints for a large farm",
+            requires:[[1,0],[2,4],[2,2]],
             cost:0,
             category:"Farming",
             unlocked: false,
@@ -109,152 +199,66 @@ const tech = [
         },
 		{
             name:"Insulae",
-            description:"Unlock a massive housing unit",
-            requires:[[1,2]],
+            description:"Unlock the blueprints for a massive housing unit",
+            requires:[[1,3],[1,4]],
             cost:0,
             category:"Housing",
             unlocked: false,
             effect(){}
         },
 		{
-            name:"Scouting",
-            description:"Unlock a new outpost building that allows you to gain information about attackers",
-            requires:[[2,2]],
+            name:"Large mine",
+            description:"Unlock the blueprints for a large mine",
+            requires:[[1,5],[1,4],[1,8]],
             cost:0,
-            category:"Military",
-            unlocked: false,
-            effect(){} 
-        },
-		{
-            name:"Stronger swords",
-            description:"All new military buildings give +20% military",
-            requires:[[2,2],[0,3]],
-            cost:0,
-            category:"Military II",
-            unlocked: false,
-            effect(){}
-        }
-	],	
-	[	//ROW 4
-		{
-            name:"Mining tacics",
-            description:"All mines are now 30% cheaper",
-            requires:[[0,2],[2,1]],
-            cost:0,
-            category:"Mining",
-            unlocked: false,
-            effect(){}
-        },
-		{
-            name:"Temple",
-            description:"Unlock a massive building to pray to RNGesus. Increases your luck",
-            requires:[[3,0],[1,0]],
-            cost:0,
-            category:"Virtue",
-            unlocked: false,
-            effect(){}
-        },
-		{
-            name:"Large Farm",
-            description:"Unlock a substantially larger farm that requires irrigation",
-            requires:[[3,0]],
-            cost:0,
-            category:"Farming",
+            category:"Mining II",
             unlocked: false,
             effect(){}
         },
 		{
             name:"Fortress",
-            description:"Unlock a massive structure that offers substantial military power",
-            requires:[[3,3]],
+            description:"Unlock the blueprints for a massive building that gives high military power",
+            requires:[[1,6]],
+            cost:0,
+            category:"Military",
+            unlocked: false,
+            effect(){}
+        },
+		{
+            name:"Temple",
+            description:"Unlock the blueprints for a temple that increases your luck at a higher rate",
+            requires:[[2,2]],
+            cost:0,
+            category:"Virtue",
+            unlocked: false,
+            effect(){}
+        }
+	],
+	[	//ROW 4
+		{
+            name:"Ultimate Megastructure",
+            description:"Collect 1 out of 3 blueprint scraps to build the Mega Temple",
+            requires:[[2,1],[2,0],[3,4]],
+            cost:0,
+            category:"Intelligence II",
+            unlocked: false,
+            effect(){}
+        },
+		{
+            name:"Archery",
+            description:"Unlock an archer tower that gives military power</br>fortresses give +20% military",
+            requires:[[2,3],[3,4]],
             cost:0,
             category:"Military II",
             unlocked: false,
             effect(){}
         },
 		{
-            name:"Cement",
-            description:"All roads and bridges now only cost 0.10 resources",
-            requires:[[2,1]],
-            cost:0,
-            category:"Intelligence",
-            unlocked: false,
-            effect(){}
-        }
-	],
-	[	//ROW 5
-		{
-            name:"Large Mine",
-            description:"Unlock a massive mine that produces considerably more resources",
-            requires:[[4,0]],
-            cost:0,
-            category:"Mining",
-            unlocked: false,
-            effect(){}
-        },
-		{
-            name:"Efficient farms",
-            description:"All farms now produce 30% more resources",
-            requires:[[3,1],[4,2]],
-            cost:0,
-            category:"Farming",
-            unlocked: false,
-            effect(){}
-        },
-		{
-            name:"Archery",
-            description:"Unlock archer towers, a cheap building that gives military power, and all fortresses give +30% military power",
-            requires:[[3,2]],
-            cost:0,
-            category:"Military",
-            unlocked: false,
-            effect(){
-			}
-        },
-		{
-            name:"Diplomacy",
-            description:"When negotiating with an enemy, gain a 60% chance that the enemy will create a peace treaty with you, decreasing chance of attacks by 40% for the next 5 years",
-            requires:[[4,1]],
+            name:"Dedicated worship",
+            description:"Temples and altars are 40% more efficient",
+            requires:[[2,2],[3,2]],
             cost:0,
             category:"Virtue",
-            unlocked: false,
-            effect(){}
-        },
-		{
-            name:"Exploration",
-            description:"Unlock infinitely more terrain to explore using WASD",
-            requires:[[4,4]],
-            cost:0,
-            category:"Intelligence",
-            unlocked: false,
-            effect(){}
-        }
-	],
-	[ // ROW 6
-		{
-            name:"Rain wanted a special name for this i forgor",
-            description:"Unlock a blueprint scrap for the Mega Temple",
-            requires:[[3,1]],
-            cost:0,
-            category:"Housing",
-            unlocked: false,
-            effect(){}
-        },
-		{
-            name:"Exploration II",
-            description:"The world border is invisible, revealing more terrain for scouting</br>roads now cost 0.05 resources instead",
-            requires:[[5,4],[3,2]],
-            cost:0,
-            category:"Intelligence",
-            unlocked: false,
-            effect(){}
-        },
-			{
-            name:"Hunting grounds",
-            description:"Unlock the ability to create hunting grounds that produce food",
-            requires:[[5,2],[3,3]],
-            cost:0,
-            category:"Military",
             unlocked: false,
             effect(){}
         }
