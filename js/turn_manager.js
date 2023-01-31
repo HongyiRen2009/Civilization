22ba3b81487853f37ca17ddcfd34a7885caed4fb
Rain Ren <59131595+HongyiRen2009@users.noreply.github.com>
2023-01-31 12:28:39 -0800

    Update turn_manager.js

--- js/turn_manager.js
+++ js/turn_manager.js
@@ -277,16 +277,12 @@ function displayUI(){
 			}
 			
 		}
-		//document.getElementById("xp_bar").style.width = 100*(xp/totalxp)+"%"
+		document.getElementById("xp_bar").style.width = 100*(xp/totalxp)+"%"
 		document.getElementById("pop").innerHTML = "Population: " + currentpop+"/"+(currentpop>population&&difficulty>5*difficultymultiplier ? "<strong class = 'color-r'>"+population+"</strong>":population)
 		document.getElementById("food").innerHTML = "Food: " + (food<currentpop ? "<strong class = 'color-r'>"+food+"</strong>": food)
 		document.getElementById("power").innerHTML = "Military: " + military
 		document.getElementById("unemployed").innerHTML = "Unemployed People: " + unemployed
-		if (resourcesgained >= 0) {
-			document.getElementById("resources").innerHTML = "Resources: " + resources + " (+" + resourcesgained + ")"
-		} else {
-			document.getElementById("resources").innerHTML = "Resources: " + resources + " (" + resourcesgained + ")"
-		}
+		document.getElementById("resources").innerHTML = "Resources: " + resources
 }
 function attack(power){
 	enemy_power = power
