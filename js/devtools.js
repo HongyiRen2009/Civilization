65008d2c076e02e39fefea5f192d3570c66fd81c
0verkil <112268976+0verkil@users.noreply.github.com>
2023-01-31 20:32:55 -0800

    devtools actually function

--- js/devtools.js
+++ js/devtools.js
@@ -1,5 +1,9 @@
 const dev = {
 
+    help: function() {
+        return dev
+    },
+
     all_tech: function() {
         if (devmode) {
             for (const techs of tech) {
@@ -7,27 +11,28 @@ const dev = {
                     technology.effect()
                 }
             }
-            console.log("Done!")
+            return "Note: Many buildings require Year > 3. Consider using time_warp()."
         } else {
-            console.log("Devmode is not enabled. Go to index.html to enable.")
+            return "Devmode is not enabled. Go to index.html to enable."
         }
     },
 
     time_warp: function() {
         if (devmode) {
             difficulty += 100
-            console.log("Done!")
+            return "Done!"
         } else {
-            console.log("Devmode is not enabled. Go to index.html to enable.")
+            return "Devmode is not enabled. Go to index.html to enable."
         }
     },
 
     give_resources: function() {
         if (devmode) {
         resources += 99999999999
-        console.log("Done!")
+        displayUI()
+        return "Done!"
         } else {
-        console.log("Devmode is not enabled. Go to index.html to enable.")
+            return "Devmode is not enabled. Go to index.html to enable."
         }
     },
 
@@ -36,10 +41,25 @@ const dev = {
             techstats.market = true
             resources += 99999999999
             research_points += 99999999999
-            console.log("Done!")
-
-            } else {
-            console.log("Devmode is not enabled. Go to index.html to enable.")
+            currentpop = 99999999999
+            gridstats.push({
+                disabled: false,
+                employmentrequired: -99999999999,
+                food: 99999999999,
+                index: "0",
+                letter: "CHEAT",
+                military: 99999999999,
+                population: 99999999999,
+                positions: [],
+                resourcerefund: 0,
+                resources: 99999999999,
+                xp: 0,
+            })
+            displayUI()
+            return "Done!"
+            
+        } else {
+            return "Devmode is not enabled. Go to index.html to enable."
         }
     
     }
