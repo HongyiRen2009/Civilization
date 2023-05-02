64ed25302089ae38174c68703ec1655624d1c8d7
Rain Ren <59131595+HongyiRen2009@users.noreply.github.com>
2023-05-02 09:37:06 -0700

    Update turn_manager.js

--- js/turn_manager.js
+++ js/turn_manager.js
@@ -102,8 +102,9 @@ function turnpopup(){
 		
 	
 
-		if (getRandomInt(0,Math.max(0,(3-Math.max(-7,Math.min(15,Math.floor((currentpop-population)/4))))*Math.min(3-difficultymultiplier,food/currentpop)+m.rebel+punishamount+(techstats.social_care ? 2:0)-outofrange)) <= 0){
-			popups[1].choosetext()
+
+
+		if (getRandomInt(0,Math.max(0,(3-Math.max(-7,Math.min(15,Math.floor((currentpop-population)/4))))*Math.min(3-difficultymultiplier,food/currentpop)+m.rebel+punishamount+(techstats.social_care ? 2:0)-outofrange)) <= 0){			popups[1].choosetext()
 			displaypopup(1)
 			return false
 
@@ -286,7 +287,7 @@ function next_turn(){
 	}
 }
 	
-	setTimeout(enable,10)
+	setTimeout(enable,1000)
 	
 }
 
@@ -454,7 +455,7 @@ function displayUI(turn=false){
 		}
 		document.getElementById("food").innerHTML = "Food: " + (food<currentpop ? "<strong class = 'color-r'>&nbsp;"+shorten(food)+"</strong>": shorten(food))
 		if (food<currentpop) {
-			displaywarning('food')
+			displaywarning('Starving')
 		} else {
 			displaywarning('food', false)
 		}
